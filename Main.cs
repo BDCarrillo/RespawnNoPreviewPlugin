8934ef2094c16cc54f456951747c56c5b529e552
BDCarrillo <90526940+BDCarrillo@users.noreply.github.com>
2023-11-24 07:40:07 -0600

    Initial commit

---
+++ Main.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+using VRage.Plugins;
+using HarmonyLib;
+
+namespace RespawnMenuNoPreview
+{
+    public class Main : IPlugin
+    {
+        public void Init(object gameInstance)
+        {
+            //new Harmony("RespawnMenuNoPreview").PatchAll(Assembly.GetExecutingAssembly());
+            new Harmony(typeof(Main).Namespace).PatchAll(typeof(Main).Assembly);
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void Dispose()
+        {
+
+        }
+    }
+}
